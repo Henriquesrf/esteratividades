8296e87b3f7df45c477dda2b8c1fc5c182b84082
Henriquesrf <henriquesantaremmoura@gmail.com>
2026-06-22 23:44:55 -0300

    first commit

---
+++ if.js
@@ -0,0 +1,109 @@
+let alunos = [
+{ nome: "Ana", idade: 17 },
+{ nome: "Carolina", idade: 7 },
+{ nome: "Juan", idade: 28 },
+{ nome: "Guilherme", idade: 13 } ];
+
+//parte 0
+
+if (alunos[0].idade < 6) {
+
+ console.log(alunos[0].nome + " Educação Infantil");
+
+} else if (alunos[0].idade > 6 && alunos[0].idade <= 10)
+
+ console.log(alunos[0].nome + " Ensino Fundamental I");
+
+else if (alunos[0].idade >= 11 && alunos[0].idade <= 14)
+
+ console.log(alunos[0].nome + " Ensino Fundamental II");
+
+else if (alunos[0].idade >= 15 && alunos[0].idade <= 17)
+
+ console.log(alunos[0].nome + " Ensino Médio");
+
+else if (alunos[0].idade >= 18)
+
+ console.log(alunos[0].nome + " Ensino Superior ou EJA");
+
+else {
+ console.log("Idade inválida");
+}
+
+//parte 1
+
+if (alunos[1].idade < 6) {
+
+ console.log(alunos[1].nome + " Educação Infantil");
+
+} else if (alunos[1].idade > 6 && alunos[1].idade <= 10)
+
+ console.log(alunos[1].nome + " Ensino Fundamental I");
+
+else if (alunos[1].idade >= 11 && alunos[1].idade <= 14)
+
+ console.log(alunos[1].nome + " Ensino Fundamental II");
+
+else if (alunos[1].idade >= 15 && alunos[1].idade <= 17)
+
+ console.log(alunos[1].nome + " Ensino Médio");
+
+else if (alunos[1].idade >= 18)
+
+ console.log(alunos[1].nome + " Ensino Superior ou EJA");
+
+else {
+ console.log("Idade inválida");
+}
+
+//parte 2
+
+if (alunos[2].idade < 6) {
+
+ console.log(alunos[2].nome + " Educação Infantil");
+
+} else if (alunos[2].idade > 6 && alunos[2].idade <= 10)
+
+ console.log(alunos[2].nome + " Ensino Fundamental I");
+
+else if (alunos[2].idade >= 11 && alunos[2].idade <= 14)
+
+ console.log(alunos[2].nome + " Ensino Fundamental II");
+
+else if (alunos[2].idade >= 15 && alunos[2].idade <= 17)
+
+ console.log(alunos[2].nome + " Ensino Médio");
+
+else if (alunos[2].idade >= 18)
+
+ console.log(alunos[2].nome + " Ensino Superior ou EJA");
+
+else {
+ console.log("Idade inválida");
+}
+
+//parte 3
+
+if (alunos[3].idade < 6) {
+
+ console.log(alunos[3].nome + " Educação Infantil");
+
+} else if (alunos[3].idade > 6 && alunos[3].idade <= 10)
+
+ console.log(alunos[3].nome + " Ensino Fundamental I");
+
+else if (alunos[3].idade >= 11 && alunos[3].idade <= 14)
+
+ console.log(alunos[3].nome + " Ensino Fundamental II");
+
+else if (alunos[3].idade >= 15 && alunos[3].idade <= 17)
+
+ console.log(alunos[3].nome + " Ensino Médio");
+
+else if (alunos[3].idade >= 18)
+
+ console.log(alunos[3].nome + " Ensino Superior ou EJA");
+
+else {
+ console.log("Idade inválida");
+}
---
+++ voto.js
@@ -0,0 +1,16 @@
+let nome_pessoa = "Ana Carolina";
+let nome_pessoa2 = "Eduardo";
+let nome_pessoa3 = "Gabriel";
+
+let idade_pessoa = 17;
+let idade_pessoa2 = 14;
+let idade_pessoa3 = 23;
+
+let resultado = idade_pessoa >=16? `Pode votar ${nome_pessoa}` : `Não pode votar ${nome_pessoa}`;
+console.log(resultado);
+
+let resultado2 = idade_pessoa2 >=16? `Pode votar ${nome_pessoa2}` : `Não pode votar ${nome_pessoa2}`;
+console.log(resultado2);
+
+let resultado3 = idade_pessoa3 >=16? `Pode votar ${nome_pessoa3}` : `Não pode votar ${nome_pessoa3}`;
+console.log(resultado3);
